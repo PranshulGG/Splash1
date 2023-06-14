8a5ccced3d41de43315c320e901c08e14853eacc
PranshulGG <134786144+PranshulGG@users.noreply.github.com>
2023-06-14 19:24:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
     setTimeout(function() {
       var splashScreen = document.querySelector('.splash-screen');
       splashScreen.parentNode.removeChild(splashScreen);
-      window.location.href = "https://example.com"; // Replace with your desired link
-    }, 15000); // Change the timeout value (in milliseconds) to your desired duration
+      window.location.href = " https://pranshulgg.github.io/splash2f/"; // Replace with your desired link
+    }, 8200); // Change the timeout value (in milliseconds) to your desired duration
   });
 
